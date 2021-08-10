c3d2368458c3e0e2ce397b684b8d3059af125b71
YNWADev <75531533+YNWADev@users.noreply.github.com>
2021-08-10 11:35:12 +0200

    Update qemu-install.sh

--- qemu-install.sh
+++ qemu-install.sh
@@ -4,7 +4,7 @@ set -xe
 
 qemu-img create -f qcow2 "$2" 4G
 
-qemu-system-x86_64 -enable-kvm -vga std \
+qemu-system-x86_64 -accel hvf -vga std \
                    -m 2048 -smp 2 -cpu host \
                    -soundhw ac97 \
                    -net nic,model=e1000 -net user \
